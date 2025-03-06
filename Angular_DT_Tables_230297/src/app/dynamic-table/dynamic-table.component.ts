8ec1bdcb0f88d48789ac0b7c2b908433a7fe67a4
Esther Gonzalez Peralta <230297@utxicotepec.edu.mx>
2025-03-06 09:40:38 -0600

    Tables
    Se arreglaron problemas en el codigo

--- Angular_DT_Tables_230297/src/app/dynamic-table/dynamic-table.component.ts
+++ Angular_DT_Tables_230297/src/app/dynamic-table/dynamic-table.component.ts
@@ -1,4 +1,10 @@
 import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import {DataTableDirective} from 'angular-datatables';
+import { Subject } from 'rxjs';
+import 'datatables.net';
+import {data} from 'jquery'
+
 
 @Component({
   selector: 'app-dynamic-table',
@@ -7,5 +13,19 @@ import { Component } from '@angular/core';
   styleUrl: './dynamic-table.component.css'
 })
 export class DynamicTableComponent {
+  users: any[]=[];
+  dtOptions: any={};
 
+  constructor(private http:HttpClient){}
+  ngOnInit(): void{
+    this.dtOptions={
+      pagingType:'full_numbers',
+      pageLenght: 5,
+      processing:true
+    };
+    this.http.get<any[]>('https://jsonplaceholder.typicode.com/users')
+    .subscribe(data =>{
+      this.users=data;
+    })
+  }
 }
